40a826da858f4e29b8da89780236a2b9c7e4aba5
scriptzzz-ops <chatgpt4676@gmail.com>
2025-08-08 17:17:44 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:4000', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
